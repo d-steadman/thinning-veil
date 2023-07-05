9f8ff4f88193550b3f785095c2ace49443cd065c
David Steadman <wil.steadman@icloud.com>
2023-07-05 13:42:38 -0400

    Entity handling functions

---
+++ entity.c
@@ -0,0 +1,37 @@
+Entity *create_entity(Position start, char symbol) {
+    Entity *new_entity = calloc(1, sizeof(Entity));
+
+    new_entity->pos.x = start.x;
+    new_entity->pos.y = start.y;
+    new_entity->symbol = symbol;
+
+    new_entity->previous = NULL;
+    new_entity->next = NULL;
+
+    return new_entity;
+}
+
+void free_entity(Entity *entity) {
+    free(entity);
+}
+
+void entity_list_insert(Entity *new_entity) {
+    // Move to last entity in list
+    Entity *current = entity_list;
+
+    while (current->next)
+        next = next->next;
+
+    new_entity->previous = current;
+    current->next = new_entity;
+}
+
+void entity_list_delete(Entity *entity) {
+    Entity *previous = entity->previous;
+    Entity *next = entity->next;
+
+    previous->next = next;
+    next->previous = previous;
+
+    free_entity(entity);
+}
--- entity.h
+++ entity.h
@@ -11,9 +11,6 @@ Also includes an entity list for holding level entities.
 #include <ncurses.h>
 #include <stdlib.h>
 
-/*
-Entity location to eliminate providing X and Y.
-*/
 typedef struct {
     int x;
     int y;
@@ -21,13 +18,14 @@ typedef struct {
 
 typedef struct {
     Position pos;
-    Direction last_move;
-
     char symbol;
+
+    // For entity list
+    Entity *previous;
+    Entity *next;
 } Entity;
 
 enum MoveDirection {
-    MOVE_DIR_FORWARD,   // Moves the same direction as the previous move
     MOVE_DIR_UP,
     MOVE_DIR_DOWN,
     MOVE_DIR_RIGHT,
@@ -40,14 +38,31 @@ enum MoveStatus {
     MOVE_STATUS_NOT_REC     // Confirm move (used for player moves)
 }
 
-// Generic entity methods
+/* Generic entity methods */
 Entity *create_entity(Position start, char symbol);
+void free_entity(Entity *entity);
+
+// No map parameter, use extern
 MoveStatus move_entity(Entity *entity, MoveDirection direction);
 
-// Player methods
+/* Player methods */
+// No player parameter, use extern
 void handle_input(int input);
+
+/*
+Entity list methods
+
+Link entities together with player as head.
+Include methods for filtering entities.
+*/
+
+// Don't need init function since we already have enemy creation
+
+void entity_list_insert(Entity *new_entity);
+
 
 // Extern variables
 extern Entity *player;
+extern Entity *entity_list;
 
 #endif
